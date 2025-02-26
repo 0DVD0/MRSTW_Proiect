a95e624ea3ce6f8d657174f2419d8928e5809658
0DVD0 <161175754+0DVD0@users.noreply.github.com>
2025-02-26 14:20:33 +0200

    added the front page of the web site

--- WebsiteGym.Web/App_Start/RouteConfig.cs
+++ WebsiteGym.Web/App_Start/RouteConfig.cs
@@ -18,6 +18,11 @@ namespace WebsiteGym.Web
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-        }
+               routes.MapRoute(
+               name: "AboutPage",
+               url: "About",
+               defaults: new { controller = "Home", action = "About" }
+);
+          }
     }
 }
