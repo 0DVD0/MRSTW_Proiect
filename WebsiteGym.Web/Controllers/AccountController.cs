6259318d6f195ff196780350c863df7b99257d49
0DVD0 <161175754+0DVD0@users.noreply.github.com>
2025-03-20 16:06:52 +0200

    Cleaned some unnecesarry code

--- WebsiteGym.Web/Controllers/AccountController.cs
+++ WebsiteGym.Web/Controllers/AccountController.cs
@@ -5,27 +5,9 @@ namespace WebsiteGym.Web.Controllers
 {
     public class AccountController : Controller
     {
-        private readonly IUserServices _userService;
-        public AccountController(IUserServices userService)
-        {
-            _userService = userService;
-        }
         public ActionResult Login()
         {
             return View();
         }
-
-        // Register user
-        [HttpPost]
-        public ActionResult Register(string name, string email, string password)
-        {
-            bool isRegistered = _userService.RegisterUser(name, email, password);
-            if (isRegistered)
-            {
-                return RedirectToAction("Login");
-            }
-            return View();
-        }
-
     }
 }
