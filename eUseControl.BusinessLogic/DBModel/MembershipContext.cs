3a2e1faf13c87449f0168557c7731f076c815528
otiliamadalina <otilia-madalina.leporda@isa.utm.md>
2025-04-02 21:17:10 +0300

    added MembershipContext.cs in BL/DBModel

---
+++ eUseControl.BusinessLogic/DBModel/MembershipContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUseControl.BusinessLogic.DBModel
+{
+    internal class MembershipContext : DbContext
+    {
+        public MembershipContex() : base("name=eUseControl")
+        {
+        }
+        public virtual DbSet<Membership> Memberships { get; set; }
+
+    }
+}
