780234b7ce13d77e3f3df481821400208e819688
otiliamadalina <otilia-madalina.leporda@isa.utm.md>
2025-04-02 21:33:53 +0300

    implemented MDbTable for membership

---
+++ eUseControl.Domain/Entities/MDbTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using eUseControl.Domain.Entities.BaseEntities;
+
+namespace eUseControl.Domain.Entities
+{
+    public class MDbTable : BaseEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        [Required]
+        [Display(Name = "Membership Name")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Membership Name is not valid")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "MembershipTimeSpan")]
+        // Timespan is for months count for membership
+        public TimeSpan TimeSpan { get; set; }
+
+        [Required]
+        [Display(Name = "Price")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Display(Name = "StartDate")]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [Display(Name = "EndDate")]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "DiscountPrice")]
+        public decimal? DiscountPrice { get; set; }
+    }
+}
