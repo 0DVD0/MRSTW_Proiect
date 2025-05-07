5941f235ac0ebb054df82d45e7d985ad1429b895
danik169 <97548569+danik169@users.noreply.github.com>
2025-05-07 21:45:47 +0300

    Added Coaches table in the database

--- eUseControl.BusinessLogic/DBModel/CoachContext.cs
+++ eUseControl.BusinessLogic/DBModel/CoachContext.cs
@@ -1,19 +1,20 @@
-﻿using eUseControl.Domain.Entities.User;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using eUseControl.Domain.Entities;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using eUseControl.Domain.Entities;
+
 
 namespace eUseControl.BusinessLogic.DBModel
 {
-     public class CoachContext: DbContext
-     {
-          public CoachContext() : base("name=eUseControl")
-          {
-          }
-          public virtual DbSet<Coach> Coaches { get; set; }
-     }
+    public class CoachContext : DbContext
+    {
+        public CoachContext() : base("name=eUseControl")
+        {
+        }
+        public virtual DbSet<Coach> Coaches { get; set; }
+
+    }
 }
